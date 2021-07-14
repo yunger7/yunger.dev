c8eb23cd3361dca90c04574abd17c41bfe8991a2
yunger <luisgalete6264@gmail.com>
2021-07-14 20:32:10 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Under construction 🔨🐒
+        </h1>
+
+        <p className={styles.description}>
+          Come back later to see this <strong>awesome</strong> website!
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
