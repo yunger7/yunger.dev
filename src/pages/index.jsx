ded7b7888a1b8c0f0d32858ce3c837df445aeef0
yunger <luisgalete6264@gmail.com>
2021-07-26 17:23:08 -0300

    remove unnecessary useTheme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ import {
 	Typography,
 	Divider,
 } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import {
 	ShoppingCart as ShoppingCartIcon,
 	LiveTv as LiveTvIcon,
@@ -118,7 +118,6 @@ const carouselProps = {
 
 export default function Home({ posts }) {
 	const classes = useStyles();
-	const theme = useTheme();
 
 	return (
 		<>
