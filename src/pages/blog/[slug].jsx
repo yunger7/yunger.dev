c3eee0ba5729737b24d64607fc3d1af2540b9d5f
yunger <luisgalete6264@gmail.com>
2021-07-30 14:20:49 -0300

    Add getStaticPaths and getStaticProps to blog post details

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,9 +1,65 @@
-import { useRouter } from "next/router"
+import { notion } from "../../services/notion";
 
-export default function BlogPost() {
-  const router = useRouter();
+export default function BlogPost({ post }) {
+  console.dir(post, { depth: null });
 
   return (
-    <h1>Blog post - {router.query.slug}</h1>
+    <h1>Blog post</h1>
   )
+}
+
+const blogDatabaseId = process.env.NOTION_BLOG_DATABASE_ID;
+
+export async function getStaticPaths() {
+  const response = await notion.databases.query({
+    database_id: blogDatabaseId,
+    filter: {
+      property: "Status",
+      select: {
+        equals: "Published",
+      },
+    },
+  });
+
+  const paths = response.results.map(page => {
+    const postSlug = page.properties["Slug"].formula.string;
+
+    return {
+      params: { slug: postSlug }
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const response = await notion.databases.query({
+    database_id: blogDatabaseId,
+    filter: {
+      and: [
+        {
+          property: "Status",
+          select: {
+            equals: "Published",
+          },
+        },
+        {
+          property: "Slug",
+          formula: {
+            text: { contains: slug },
+          },
+        },
+      ],
+    },
+    page_size: 1,
+  });
+
+  const post = response.results[0];
+
+  return {
+    props: { post }
+  };
 }
