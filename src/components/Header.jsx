2571671ee6cfd8631e1c66235dd9dbc5627acaef
yunger <luisgalete6264@gmail.com>
2021-07-30 19:21:03 -0300

    Change Header component to accept a paths prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import _ from "lodash";
-import cs from "classnames";
 import {
 	AppBar,
 	Toolbar,
 	Breadcrumbs,
 	Button,
 	Link as MuiLink,
+	CssBaseline,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Search as SearchIcon, Home as HomeIcon } from "@material-ui/icons";
+import { Search as SearchIcon } from "@material-ui/icons";
+
+import cs from "classnames";
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -25,12 +25,12 @@ const useStyles = makeStyles(theme => ({
 		flexGrow: 1,
 	},
 	link: {
-    padding: ".1rem .35rem",
-    fontSize: 14,
+		padding: ".1rem .35rem",
+		fontSize: 14,
 		cursor: "pointer",
-    display: "flex",
-    alignItems: "flex-end",
-    borderRadius: theme.shape.borderRadius,
+		display: "flex",
+		alignItems: "flex-end",
+		borderRadius: theme.shape.borderRadius,
 	},
 	linkDisabled: {
 		cursor: "default",
@@ -43,56 +43,67 @@ const useStyles = makeStyles(theme => ({
 		width: 20,
 		height: 20,
 	},
-  searchButton: {
-    fontSize: 14,
-    padding: ".1rem .5rem",
-  },
+	searchButton: {
+		fontSize: 14,
+		padding: ".1rem .5rem",
+	},
 }));
 
-export function Header() {
-	const router = useRouter();
-	const classes = useStyles();
+const defaultPaths = [
+	{
+		name: "yunger.dev",
+		href: "/",
+	},
+]
 
-	const pathnames = router.pathname.split("/").filter(x => x);
+export function Header({ paths = defaultPaths }) {
+	const classes = useStyles();
 
 	return (
 		<AppBar className={classes.root} position="sticky" color="inherit">
 			<Toolbar className={classes.toolbar}>
 				<Breadcrumbs className={classes.breadcrumbs} aria-label="breadcrumb">
-					<Link href="/" passHref>
+					{paths.length > 1 ? (
+						paths.map((path, index) => {
+							if (index === paths.length - 1) {
+								return (
+									<MuiLink
+										className={cs(classes.link, classes.linkDisabled)}
+										color="inherit"
+										key={path.name}
+									>
+										{path.name}
+									</MuiLink>
+								);
+							}
+
+							return (
+								<Link href={path.href} passHref key={path.name}>
+									<MuiLink
+										className={classes.link}
+										color="inherit"
+									>
+										{path.name}
+									</MuiLink>
+								</Link>
+							);
+						})
+					) : (
 						<MuiLink
-							className={cs(classes.link, {
-								[classes.linkDisabled]: !pathnames.length,
-							})}
+							className={cs(classes.link, classes.linkDisabled)}
 							color="inherit"
 						>
-							<HomeIcon className={classes.linkIcon} />
 							yunger.dev
 						</MuiLink>
-					</Link>
-					{pathnames.map((path, index) => {
-						const isLast = index === pathnames.length - 1;
-						const href = `/${pathnames.slice(0, index + 1).join("/")}`;
-
-						return isLast ? (
-							<MuiLink
-								className={cs(classes.link, { [classes.linkDisabled]: isLast })}
-								color="inherit"
-								key={href}
-							>
-								{_.capitalize(path)}
-							</MuiLink>
-						) : (
-							<Link href={href} key={href} passHref>
-								<MuiLink className={classes.link} color="inherit">
-									{_.capitalize(path)}
-								</MuiLink>
-							</Link>
-						);
-					})}
+					)}
 				</Breadcrumbs>
 
-				<Button className={classes.searchButton} size="small" startIcon={<SearchIcon />} disableRipple>
+				<Button
+					className={classes.searchButton}
+					size="small"
+					startIcon={<SearchIcon />}
+					disableRipple
+				>
 					Search
 				</Button>
 			</Toolbar>
