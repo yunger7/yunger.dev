41c7320fa61128ac4c9979e6ffac495ca0155c0d
yunger <luisgalete6264@gmail.com>
2021-07-30 19:56:37 -0300

    Add blog post introduction section

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,3 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import {
+	Container,
+	Card,
+	CardContent,
+	Typography,
+	Chip,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 import { notion } from "../../services/notion";
 
 import { getNordColor } from "../../utils/getNordColor";
@@ -5,8 +17,39 @@ import { getPostCoverImage } from "../../utils/getPostCoverImage";
 import { getPlainTextFromBlocks } from "../../utils/getPlainTextFromBlocks";
 
 import { Header } from "../../components/Header";
+import { RichText } from "../../components/RichText";
+
+import placeholder3 from "../../../public/placeholder3.jpg";
+
+const useStyles = makeStyles(theme => ({
+	page: {
+		paddingTop: theme.spacing(6),
+	},
+	blogImage: {
+		width: "100%",
+		height: 250,
+		position: "relative",
+	},
+	tags: {
+		margin: `${theme.spacing(1.5)}px 0`,
+
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	tag: {
+		margin: `0 ${theme.spacing(0.5)}px`,
+	},
+	date: {
+		fontWeight: 400,
+	},
+}));
 
 export default function BlogPost({ post }) {
+	const classes = useStyles();
+	const { title, image, slug, tags, createdAt } = post;
+	const titleAsPlainText = getPlainTextFromBlocks(title);
+
 	console.dir(post, { depth: null });
 
 	const headerPaths = [
@@ -19,14 +62,73 @@ export default function BlogPost({ post }) {
 			href: "/blog",
 		},
 		{
-			name: getPlainTextFromBlocks(post.title),
-			href: post.slug,
+			name: titleAsPlainText,
+			href: slug,
 		},
-	]
+	];
 
 	return (
 		<>
+			<Head>
+				<title>{titleAsPlainText}</title>
+			</Head>
 			<Header paths={headerPaths} />
+			<main className={classes.page}>
+				<section>
+					<Container maxWidth="lg">
+						<Card>
+							<div className={classes.blogImage}>
+								{image ? (
+									<Image
+										src={image.src}
+										alt="Post cover"
+										layout="fill"
+										objectFit="cover"
+										placeholder="blur"
+										blurDataURL={image.blurDataURL}
+									/>
+								) : (
+									<Image
+										src={placeholder3}
+										alt="Post cover"
+										layout="fill"
+										objectFit="cover"
+										placeholder="blur"
+									/>
+								)}
+							</div>
+							<CardContent>
+								<Typography component="h1" variant="h5" align="center">
+									<RichText text={title} />
+								</Typography>
+								<div className={classes.tags}>
+									{tags.map(({ id, name, color }) => (
+										<Chip
+											className={classes.tag}
+											size="small"
+											label={name}
+											style={{ backgroundColor: color }}
+											key={id}
+										/>
+									))}
+								</div>
+								<Typography
+									className={classes.blogDate}
+									variant="subtitle2"
+									align="center"
+									gutterBottom
+								>
+									{new Date(createdAt).toLocaleDateString("en-US", {
+										month: "short",
+										day: "numeric",
+										year: "numeric",
+									})}
+								</Typography>
+							</CardContent>
+						</Card>
+					</Container>
+				</section>
+			</main>
 		</>
 	);
 }
@@ -81,7 +183,7 @@ export async function getStaticProps({ params: { slug } }) {
 	});
 
 	const notionPost = dataResponse.results[0];
-  const postImage = await getPostCoverImage(notionPost);
+	const postImage = await getPostCoverImage(notionPost);
 
 	const post = {
 		id: notionPost.id,
@@ -101,11 +203,11 @@ export async function getStaticProps({ params: { slug } }) {
 		notionUrl: notionPost.url,
 	};
 
-  const pageResponse = await notion.blocks.children.list({
-    block_id: post.id
-  });
+	const pageResponse = await notion.blocks.children.list({
+		block_id: post.id,
+	});
 
-  post.content = pageResponse.results;
+	post.content = pageResponse.results;
 
 	return {
 		props: { post },
