d0f1daab5b22a8388b03ebf8ed5c0118facaabec
yunger <luisgalete6264@gmail.com>
2021-08-06 16:39:27 -0300

    Add Header to About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,6 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 import { Navbar } from "../components/Navbar";
 import { WaveDivider4 } from "../components/dividers/WaveDivider4";
-import { WaveDivider1 } from "../components/dividers/WaveDivider1";
 import { Footer } from "../components/Footer";
 
 import { getNotionPageContent } from "../lib/getNotionPageContent";
@@ -14,6 +13,7 @@ import { useBlockRenderer } from "../hooks/useBlockRenderer";
 
 import nordicWallpaper from "../../public/nordic-wallpaper.jpg";
 import profilePicture from "../../public/profile-picture.jpg";
+import { Header } from "../components/Header";
 
 const navbarPaths = [
 	{
@@ -27,53 +27,15 @@ const navbarPaths = [
 ];
 
 const useStyles = makeStyles(theme => ({
-	page: {
-		paddingTop: theme.spacing(6),
-	},
-	introduction: {
-		paddingBottom: theme.spacing(5),
-		[theme.breakpoints.down("sm")]: {
-			paddingBottom: theme.spacing(10),
-		},
-	},
-	introductionCard: {
-		width: "100%",
-	},
-	introductionWallpaper: {
-		width: "100%",
-		height: 250,
-		position: "relative",
-	},
-	introductionCardContent: {
-		padding: `${theme.spacing(2)}px ${theme.spacing(5)}px`,
-	},
-	profile: {
-		display: "flex",
-		[theme.breakpoints.down("sm")]: {
-			flexDirection: "column",
-			alignItems: "center",
-			textAlign: "center",
-			marginBottom: theme.spacing(2),
-		},
-	},
-	profileName: {
-		display: "flex",
-		flexDirection: "column",
-	},
 	profileImage: {
-		position: "relative",
-		bottom: 100,
-		maxHeight: 100,
-		marginRight: theme.spacing(2.5),
+		marginBottom: theme.spacing(1),
+
 		"& img": {
-			borderRadius: theme.shape.borderRadius,
-		},
-		[theme.breakpoints.down("sm")]: {
-			marginRight: 0,
+			borderRadius: 10,
 		},
 	},
 	content: {
-		paddingTop: theme.spacing(2.5),
+		paddingTop: theme.spacing(5),
 		paddingBottom: theme.spacing(10),
 		backgroundColor: theme.palette.background.paper,
 	},
@@ -92,48 +54,38 @@ export default function About({ content }) {
 
 			<Navbar paths={navbarPaths} />
 
+			<Header
+				backgroundImage={
+					<Image
+						src={nordicWallpaper}
+						alt="Nordic Wallpaper"
+						layout="fill"
+						placeholder="blur"
+						objectFit="cover"
+						priority
+					/>
+				}
+				dividerColor={theme.palette.background.paper}
+			>
+				<div className={classes.profileImage}>
+					<Image
+						priority
+						src={profilePicture}
+						alt="yunger profile image"
+						width={150}
+						height={150}
+					/>
+				</div>
+				<Typography variant="h3" component="h1">
+					About me
+				</Typography>
+				<Typography variant="subtitle1">
+					A little bit of info about <strong>yunger</strong>, the
+					creator behind this website.
+				</Typography>
+			</Header>
+
 			<main className={classes.page}>
-				<section className={classes.introduction}>
-					<Container maxWidth="lg">
-						<Card className={classes.introductionCard}>
-							<div className={classes.introductionWallpaper}>
-								<Image
-									src={nordicWallpaper}
-									alt="Snow mountain wallpaper"
-									layout="fill"
-									placeholder="blur"
-									objectFit="cover"
-									priority
-								/>
-							</div>
-							<CardContent className={classes.introductionCardContent}>
-								<div className={classes.profile}>
-									<div className={classes.profileImage}>
-										<Image
-											src={profilePicture}
-											alt="yunger profile image"
-											width={175}
-											height={175}
-											priority
-										/>
-									</div>
-									<div className={classes.profileName}>
-										<Typography variant="h3" component="h1">
-											About me
-										</Typography>
-										<Typography variant="subtitle1">
-											A little bit of info about <strong>yunger</strong>, the
-											creator behind this website.
-										</Typography>
-									</div>
-								</div>
-							</CardContent>
-						</Card>
-					</Container>
-				</section>
-
-				<WaveDivider1 background={theme.palette.background.paper} />
-
 				<section className={classes.content}>
 					<Container maxWidth="md">{jsxContent}</Container>
 				</section>
