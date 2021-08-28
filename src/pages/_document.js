78a1c74a79e501d8420838ffabf7e7b5de52fc98
yunger <luisgalete6264@gmail.com>
2021-08-28 16:10:45 -0300

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
 					/>
+					<link rel="shortcut icon" href="/favicon.ico" />
 				</Head>
 				<body>
 					<Main />
