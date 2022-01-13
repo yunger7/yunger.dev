032f688e2b8186466608ff5af7ec8bab54f43e81
yunger <luisgalete6264@gmail.com>
2022-01-13 14:43:52 -0300

    Remove makeStyles from 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,68 +2,67 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Button, Typography } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
+import { Box, Button, Typography } from "@mui/material";
+import { alpha } from "@mui/material/styles";
 import { Home as HomeIcon } from "@mui/icons-material";
 
 import { palette } from "../theme";
 
 import placeholder2 from "../../public/placeholder2.jpg";
 
-const useStyles = makeStyles({
-	page: {
-		position: "absolute",
-		width: "100vw",
-		height: "100vh",
-		overflow: "hidden",
-		backgroundColor: `${palette.nord0}aa`,
-
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	image: {
-		position: "relative",
-		width: "100vw",
-		height: "100vh",
-		zIndex: -1,
-		filter: "blur(10px)",
-		userSelect: "none",
-	},
-	line: {
-		display: "inline-block",
-	},
-});
-
 export default function NotFound() {
-	const classes = useStyles();
-
 	return (
 		<>
 			<Head>
 				<title>404 | Page not found</title>
 			</Head>
-			<main className={classes.page}>
-				<Typography variant="h1" align="center" gutterBottom>
-					<span className={classes.line}>(404) Woops </span>{" "}
-					<span className={classes.line}>(っ °Д °;)っ</span>
+			<Box
+				component="main"
+				sx={{
+					position: "absolute",
+					width: "100vw",
+					height: "100vh",
+					overflow: "hidden",
+					bgcolor: alpha(palette.nord0, 0.6),
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Typography variant="h1" align="center">
+					<Box component="span" sx={{ display: "inline-block" }}>
+						Woops{" "}
+					</Box>{" "}
+					<Box component="span" sx={{ display: "inline-block" }}>
+						(っ °Д °;)っ
+					</Box>
 				</Typography>
-				<Typography variant="h5" align="center" gutterBottom>
+				<Typography variant="h5" align="center">
 					Hurry up and go home before you catch a cold!
 				</Typography>
-				<Link href="/" passHref>
+				<Link passHref href="/">
 					<Button
+						disableRipple
 						variant="contained"
 						color="primary"
 						startIcon={<HomeIcon />}
-						disableRipple
+						sx={{ mt: 2 }}
 					>
 						Go home
 					</Button>
 				</Link>
-			</main>
-			<div className={classes.image}>
+			</Box>
+			<Box
+				sx={{
+					position: "relative",
+					width: "100vw",
+					height: "100vh",
+					zIndex: -1,
+					filter: "blur(10px)",
+					userSelect: "none",
+				}}
+			>
 				<Image
 					priority
 					src={placeholder2}
@@ -72,7 +71,7 @@ export default function NotFound() {
 					objectFit="cover"
 					placeholder="blur"
 				/>
-			</div>
+			</Box>
 		</>
 	);
 }
