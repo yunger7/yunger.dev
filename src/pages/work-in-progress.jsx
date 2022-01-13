1ce8f809bf3a2f1982094c1e2e08b1843a5ebf90
yunger <luisgalete6264@gmail.com>
2022-01-13 14:44:31 -0300

    Change background alpha from 0.7 to 0.6

--- src/pages/work-in-progress.jsx
+++ src/pages/work-in-progress.jsx
@@ -25,7 +25,7 @@ export default function WorkInProgress() {
 					width: "100vw",
 					height: "100vh",
 					overflow: "hidden",
-					background: alpha(palette.nord0, 0.7),
+					background: alpha(palette.nord0, 0.6),
 					display: "flex",
 					flexDirection: "column",
 					alignItems: "center",
