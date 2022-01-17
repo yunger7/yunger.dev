bf887f4ef34d00e69a86eedecfa8bdf33fdb90ff
yunger <luisgalete6264@gmail.com>
2022-01-17 18:34:20 -0300

    Fix SSR of MUI components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,33 @@
-import "../styles/globals.css";
+import { CacheProvider } from "@emotion/react";
+import PropTypes from "prop-types";
 
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 
+import { createEmotionCache } from "../lib/createEmotionCache";
 import { dark } from "../theme";
 
-export default function App({ Component, pageProps }) {
+import "../styles/globals.css";
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props) {
+	const { Component, pageProps, emotionCache = clientSideEmotionCache } = props;
+
 	return (
-		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={dark}>
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</StyledEngineProvider>
+		<CacheProvider value={emotionCache}>
+			<StyledEngineProvider injectFirst>
+				<ThemeProvider theme={dark}>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</StyledEngineProvider>
+		</CacheProvider>
 	);
 }
+
+App.propTypes = {
+	Component: PropTypes.elementType.isRequired,
+	emotionCache: PropTypes.object,
+	pageProps: PropTypes.object.isRequired,
+};
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,9 @@
-import React from "react";
+import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
+import createEmotionServer from "@emotion/server/create-instance";
+
+// import { base as theme } from "../theme";
+import { createEmotionCache } from "../lib/createEmotionCache";
 
 export default class MyDocument extends Document {
 	render() {
@@ -18,6 +21,8 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 					/>
 					<link rel="shortcut icon" href="/favicon.ico" />
+					{/* Inject MUI styles first to match with the prepend: true configuration. */}
+					{this.props.emotionStyleTags}
 				</Head>
 				<body>
 					<Main />
@@ -53,23 +58,36 @@ MyDocument.getInitialProps = async ctx => {
 	// 3. app.render
 	// 4. page.render
 
-	// Render app and page and get the context of the page with collected side effects.
-	const sheets = new ServerStyleSheets();
 	const originalRenderPage = ctx.renderPage;
+
+	// You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+	// However, be aware that it can have global side effects.
+	const cache = createEmotionCache();
+	const { extractCriticalToChunks } = createEmotionServer(cache);
 
 	ctx.renderPage = () =>
 		originalRenderPage({
-			enhanceApp: App => props => sheets.collect(<App {...props} />),
+			enhanceApp: App =>
+				function EnhanceApp(props) {
+					return <App emotionCache={cache} {...props} />;
+				},
 		});
 
 	const initialProps = await Document.getInitialProps(ctx);
+	// This is important. It prevents emotion to render invalid HTML.
+	// See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+	const emotionStyles = extractCriticalToChunks(initialProps.html);
+	const emotionStyleTags = emotionStyles.styles.map(style => (
+		<style
+			data-emotion={`${style.key} ${style.ids.join(" ")}`}
+			key={style.key}
+			// eslint-disable-next-line react/no-danger
+			dangerouslySetInnerHTML={{ __html: style.css }}
+		/>
+	));
 
 	return {
 		...initialProps,
-		// Styles fragment is rendered after the app and page rendering finish.
-		styles: [
-			...React.Children.toArray(initialProps.styles),
-			sheets.getStyleElement(),
-		],
+		emotionStyleTags,
 	};
 };
