884a41f0c0fe6902edf920999ba630fa3e611947
yunger <luisgalete6264@gmail.com>
2022-01-17 15:05:43 -0300

    Add Divider to Footer

---
+++ src/components/Footer/Divider.jsx
@@ -0,0 +1,55 @@
+import { styled } from "@mui/material/styles";
+
+const Svg = styled("svg", {
+	shouldForwardProp: prop => prop !== "backgroundColor",
+})(({ theme, backgroundColor }) => ({
+	display: "block",
+	bottom: -1,
+	left: 0,
+	right: 0,
+	width: "100%",
+	backgroundColor: backgroundColor
+		? `${backgroundColor} !important`
+		: theme.palette.background.paper,
+	pointerEvents: "none",
+	userSelect: "none",
+	verticalAlign: "middle",
+	overflow: "hidden",
+
+	"& > *": {
+		transition: "fill 400ms ease-in-out 0s",
+	},
+
+	"& .layer-1": {
+		fill: "#373d4c",
+	},
+	"& .layer-2": {
+		fill: "#39404f",
+	},
+	"& .layer-3": {
+		fill: "#3b4252",
+	},
+}));
+
+export function Divider({ backgroundColor }) {
+	return (
+		<Svg
+			viewBox="0 0 1920 250"
+			xmlns="http://www.w3.org/2000/svg"
+			backgroundColor={backgroundColor}
+		>
+			<path
+				d="M1920 250H0V0s126.707 78.536 349.975 80.05c177.852 1.203 362.805-63.874 553.803-63.874 290.517 0 383.458 57.712 603.992 61.408 220.527 3.696 278.059-61.408 412.23-17.239"
+				className="layer-1"
+			></path>
+			<path
+				d="M1920 144s-467.917 116.857-1027.243-17.294C369.986 1.322 0 45.578 0 45.578V250h1920V144z"
+				className="layer-2"
+			></path>
+			<path
+				d="M0 195.553s208.547-75.581 701.325-20.768c376.707 41.908 520.834-67.962 722.545-67.962 222.926 0 311.553 83.523 496.129 86.394V250H0v-54.447z"
+				className="layer-3"
+			></path>
+		</Svg>
+	);
+}
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,78 +6,82 @@ import {
 
 import { Links } from "./Links";
 import { SocialMedia } from "./SocialMedia";
+import { Divider } from "./Divider";
 
 import { palette } from "../../theme";
 
-export default function Footer() {
+export default function Footer({ backgroundColor }) {
 	return (
-		<Box
-			component="footer"
-			sx={{
-				minHeight: 300,
-				pt: 2.5,
-				pb: 5,
-				bgcolor: palette.nord1,
-			}}
-		>
-			<Container
-				maxWidth="lg"
-				sx={theme => ({
-					height: 1,
-					display: "flex",
-					justifyContent: "space-between",
-					[theme.breakpoints.down("md")]: {
-						flexDirection: "column",
-						alignItems: "center",
-						gap: theme.spacing(5),
-					},
-				})}
+		<Box component="footer">
+			<Divider backgroundColor={backgroundColor} />
+			<Box
+				sx={{
+					minHeight: 300,
+					pt: 2.5,
+					pb: 5,
+					bgcolor: palette.nord1,
+				}}
 			>
-				<Box
-					sx={{
-						width: { xs: "65%", sm: "30%", md: "20%" },
-						textAlign: { xs: "center", md: "left" },
-					}}
+				<Container
+					maxWidth="lg"
+					sx={theme => ({
+						height: 1,
+						display: "flex",
+						justifyContent: "space-between",
+						[theme.breakpoints.down("md")]: {
+							flexDirection: "column",
+							alignItems: "center",
+							gap: theme.spacing(5),
+						},
+					})}
 				>
-					<div>
-						<Typography
-							variant="h5"
-							sx={{
-								fontWeight: 500,
-								mb: { xs: 2, md: 1.15 },
-							}}
-						>
-							yunger.dev
-						</Typography>
-						<SocialMedia />
-					</div>
-					<Box sx={{ color: palette.nord4 }}>
-						<Typography variant="body2">
-							&copy; 2021 - {new Date().getFullYear()} yunger
-						</Typography>
-						<Typography variant="body2">
-							Build with <FavoriteIcon color="primary" sx={{ fontSize: 14 }} />{" "}
-							and JavaScript
-						</Typography>
-						<Typography variant="body2">v0.1.0 (In development)</Typography>
-					</Box>
-					<Button
-						variant="outlined"
-						color="primary"
-						href="https://ko-fi.com/yunger"
-						target="_blank"
-						rel="noreferrer"
-						startIcon={<LocalCafeIcon />}
+					<Box
 						sx={{
-							mt: 3,
-							width: 1,
+							width: { xs: "65%", sm: "30%", md: "20%" },
+							textAlign: { xs: "center", md: "left" },
 						}}
 					>
-						Buy me a coffee
-					</Button>
-				</Box>
-				<Links />
-			</Container>
+						<div>
+							<Typography
+								variant="h5"
+								sx={{
+									fontWeight: 500,
+									mb: { xs: 2, md: 1.15 },
+								}}
+							>
+								yunger.dev
+							</Typography>
+							<SocialMedia />
+						</div>
+						<Box sx={{ color: palette.nord4 }}>
+							<Typography variant="body2">
+								&copy; 2021 - {new Date().getFullYear()} yunger
+							</Typography>
+							<Typography variant="body2">
+								Build with{" "}
+								<FavoriteIcon color="primary" sx={{ fontSize: 14 }} /> and
+								JavaScript
+							</Typography>
+							<Typography variant="body2">v0.1.0 (In development)</Typography>
+						</Box>
+						<Button
+							variant="outlined"
+							color="primary"
+							href="https://ko-fi.com/yunger"
+							target="_blank"
+							rel="noreferrer"
+							startIcon={<LocalCafeIcon />}
+							sx={{
+								mt: 3,
+								width: 1,
+							}}
+						>
+							Buy me a coffee
+						</Button>
+					</Box>
+					<Links />
+				</Container>
+			</Box>
 		</Box>
 	);
 }
