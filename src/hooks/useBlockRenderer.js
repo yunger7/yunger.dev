abf31e7732eb173aaa4576e939c086bafac3fb67
yunger <luisgalete6264@gmail.com>
2022-05-01 17:05:13 -0300

    Add id to blocks

--- src/hooks/useBlockRenderer.js
+++ src/hooks/useBlockRenderer.js
@@ -16,6 +16,7 @@ import {
 } from "@mui/material";
 
 import { getSyntaxHighlightingLanguage } from "@lib/getSyntaxHighlightingLanguage";
+import { UUID } from "@utils";
 import { RichText } from "@components";
 
 export function useBlockRenderer(blocks) {
@@ -29,31 +30,33 @@ export function useBlockRenderer(blocks) {
 }
 
 function renderBlock(block) {
-	const { type, id } = block;
+	const { type, id: uuid } = block;
 	const value = block[type];
+
+	const id = UUID.removeDashes(uuid);
 
 	switch (type) {
 		case "paragraph":
 			return (
-				<Typography paragraph variant="body1" key={id}>
+				<Typography paragraph variant="body1" id={id} key={id}>
 					<RichText text={value.rich_text} />
 				</Typography>
 			);
 		case "heading_1":
 			return (
-				<Typography variant="h3" component="h2" key={id} sx={{ mt: 5 }}>
+				<Typography variant="h3" component="h2" id={id} key={id} sx={{ mt: 5 }}>
 					<RichText text={value.rich_text} />
 				</Typography>
 			);
 		case "heading_2":
 			return (
-				<Typography variant="h4" component="h3" key={id} sx={{ mt: 4 }}>
+				<Typography variant="h4" component="h3" id={id} key={id} sx={{ mt: 4 }}>
 					<RichText text={value.rich_text} />
 				</Typography>
 			);
 		case "heading_3":
 			return (
-				<Typography variant="h5" component="h4" key={id} sx={{ mt: 3 }}>
+				<Typography variant="h5" component="h4" id={id} key={id} sx={{ mt: 3 }}>
 					<RichText text={value.rich_text} />
 				</Typography>
 			);
@@ -71,6 +74,7 @@ function renderBlock(block) {
 					overlayBgColorStart="rgba(0, 0, 0, 0.5)"
 					overlayBgColorEnd="rgba(0, 0, 0, 0.5)"
 					wrapStyle={{ width: "100%" }}
+					id={id}
 					key={id}
 				>
 					<Box sx={{ position: "relative", height: 500, width: 1 }}>
@@ -88,6 +92,7 @@ function renderBlock(block) {
 			return (
 				<Box
 					component="li"
+					id={id}
 					key={id}
 					sx={{
 						listStyle: "none",
@@ -107,7 +112,7 @@ function renderBlock(block) {
 			);
 		case "to_do":
 			return (
-				<Box key={id} sx={{ my: 0.5, mx: 0 }}>
+				<Box id={id} key={id} sx={{ my: 0.5, mx: 0 }}>
 					<FormControlLabel
 						control={
 							<Checkbox
@@ -134,7 +139,7 @@ function renderBlock(block) {
 			);
 		case "toggle":
 			return (
-				<Accordion key={id}>
+				<Accordion id={id} key={id}>
 					<AccordionSummary>
 						<RichText text={value.rich_text} />
 					</AccordionSummary>
@@ -149,7 +154,7 @@ function renderBlock(block) {
 				.join("");
 
 			return (
-				<Paper elevation={4} key={id}>
+				<Paper elevation={4} id={id} key={id}>
 					<SyntaxHighlighter
 						showLineNumbers
 						language={getSyntaxHighlightingLanguage(value.language)}
@@ -160,7 +165,12 @@ function renderBlock(block) {
 				</Paper>
 			);
 		case "child_page":
-			return <p key={id}>{value.title}</p>; // Temp
+			// Temp
+			return (
+				<p id={id} key={id}>
+					{value.title}
+				</p>
+			);
 		default:
 			return (
 				<p key={id}>
