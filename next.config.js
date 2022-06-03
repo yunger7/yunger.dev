043c03448e7fb690e7b6260bf01cddb322b9bcbb
Luís Galete <luisgalete6264@gmail.com>
2022-06-03 20:42:04 +0000

    Add 'assets' rewrite

--- next.config.js
+++ next.config.js
@@ -3,24 +3,33 @@ module.exports = {
 		domains: ["i.imgur.com", "www.notion.so", "images.unsplash.com"],
 	},
 	async rewrites() {
-		return [
-			{
-				source: "/projects",
-				destination: "/work-in-progress",
-			},
-			{
-				source: "/notes",
-				destination: "/work-in-progress",
-			},
-			{
-				source: "/animelist",
-				destination: "/work-in-progress",
-			},
-			{
-				source: "/utils",
-				destination: "/work-in-progress",
-			},
-		];
+		return {
+			beforeFiles: [
+				{
+					source: "/:asset*",
+					has: [{ type: "host", value: "assets.yunger.dev" }],
+					destination: "/:asset*",
+				},
+			],
+			afterFiles: [
+				{
+					source: "/projects",
+					destination: "/work-in-progress",
+				},
+				{
+					source: "/notes",
+					destination: "/work-in-progress",
+				},
+				{
+					source: "/animelist",
+					destination: "/work-in-progress",
+				},
+				{
+					source: "/utils",
+					destination: "/work-in-progress",
+				},
+			],
+		};
 	},
 	async redirects() {
 		return [
